d61c0633324ec9fa1289405e0aa75959a734c3d3
Andy Jimenez <a17100657@gmail.com>
2024-06-13 19:08:12 -0600

    Agregar vistas usuario
    Citas, catalogo y perfil

--- app/src/main/java/com/example/happypets/ui/citas/CitasFragment.kt
+++ app/src/main/java/com/example/happypets/ui/citas/CitasFragment.kt
@@ -28,10 +28,7 @@ class CitasFragment : Fragment() {
         _binding = FragmentCitasBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
