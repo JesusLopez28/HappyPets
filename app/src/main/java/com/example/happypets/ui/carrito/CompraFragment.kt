78867bb246c7ac416153fcf0c4a06a4cd3cdab61
Andy Jimenez <a17100657@gmail.com>
2024-06-14 21:49:19 -0600

    Agregar CardView y Fragment
    Crear cardView de carrito y fragment de compra y corregir detalles

---
+++ app/src/main/java/com/example/happypets/ui/carrito/CompraFragment.kt
@@ -0,0 +1,37 @@
+package com.example.happypets.ui.carrito
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.happypets.databinding.FragmentCarritoBinding
+import com.example.happypets.databinding.FragmentCompraBinding
+
+class CompraFragment : Fragment() {
+
+    private var _binding: FragmentCompraBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentCompraBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
