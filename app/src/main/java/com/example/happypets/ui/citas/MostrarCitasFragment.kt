54789f3e6a7d77967bb8b7a53aae81aba6af82f2
Andy Jimenez <a17100657@gmail.com>
2024-06-14 23:15:31 -0600

    Crear Fragment para mostrar citas

---
+++ app/src/main/java/com/example/happypets/ui/citas/MostrarCitasFragment.kt
@@ -0,0 +1,35 @@
+package com.example.happypets.ui.citas
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.happypets.databinding.FragmentMostrarCitasBinding
+
+class MostrarCitasFragment : Fragment() {
+
+    private var _binding: FragmentMostrarCitasBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentMostrarCitasBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
