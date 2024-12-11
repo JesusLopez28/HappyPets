287fd9d03f4d24c81c05d5759a091c0f809f1104
JesusLopez28 <jesus.lopez280402@gmail.com>
2024-12-11 08:16:39 -0600

    FIN

--- app/src/main/java/com/example/happypets/ui/citas_admin/CitasAdminFragment.kt
+++ app/src/main/java/com/example/happypets/ui/citas_admin/CitasAdminFragment.kt
@@ -1,10 +1,8 @@
 package com.example.happypets.ui.citas_admin
 
 import android.content.Intent
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,73 +13,132 @@ import android.widget.ImageButton
 import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
-import com.example.happypets.Cita
-import com.example.happypets.CitaManager
+import androidx.fragment.app.Fragment
+import com.example.happypets.Config
 import com.example.happypets.R
-import com.example.happypets.UserManager
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
 class CitasAdminFragment : Fragment() {
 
-    private lateinit var citasManager: CitaManager
-    private lateinit var calendario : CalendarView
-    private lateinit var buscar : Button
-    private lateinit var listaCitas : ListView
+    private lateinit var calendario: CalendarView
+    private lateinit var buscar: Button
+    private lateinit var listaCitas: ListView
     private lateinit var btnScanQR: ImageButton
-    private lateinit var textFieldCitasEscaneadas : TextView
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        citasManager = CitaManager(requireContext())
-
-
-    }
+    private lateinit var textFieldCitasEscaneadas: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         val view = inflater.inflate(R.layout.fragment_citas_admin, container, false)
+
         calendario = view.findViewById(R.id.CalendarioAdmin)
         buscar = view.findViewById(R.id.BuscarCitaButton)
         listaCitas = view.findViewById(R.id.ListaCitasAdmin)
         btnScanQR = view.findViewById(R.id.imageButton)
-
         textFieldCitasEscaneadas = view.findViewById(R.id.textoCitaEscaneada)
 
-        // Configurar el botón para escanear
+        var selectedDate = ""
+
+        // Configuración del calendario
+        calendario.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            selectedDate = String.format("%02d/%02d/%04d", dayOfMonth, month + 1, year)
+        }
+
+        // Configurar el botón para buscar citas
+        buscar.setOnClickListener {
+            if (selectedDate.isEmpty()) {
+                Toast.makeText(requireContext(), "Seleccione una fecha", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            fetchCitas(selectedDate)
+        }
+
+        // Configurar el botón para escanear QR
         btnScanQR.setOnClickListener {
             iniciarEscaneoQR()
         }
 
-        var slectedDate = ""
-        calendario.setOnDateChangeListener { _, year, month, dayOfMonth ->
-            slectedDate = String.format("%02d/%02d/%04d", dayOfMonth, month + 1, year)
-        }
-
-        // Buscar citas para la fecha seleccionada
-        buscar.setOnClickListener {
-            val fecha = slectedDate
-            val citas = citasManager.getCitasByDate(requireContext(), fecha)
-            if (citas.isEmpty()) {
-                listaCitas.adapter = null
-                Toast.makeText(requireContext(), "No hay citas para esta fecha", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            // Ir llenado un adapter con las citas encontradas
-            val citasList = mutableListOf<String>()
-            for (cita in citas) {
-                val citaString = "Usuario: ${cita.usuario.nombre}, Mascota: ${cita.mascota.nombre}, Hora: ${cita.hora}"
-                citasList.add(citaString)
-            }
-
-            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, citasList)
-            listaCitas.adapter = adapter
-        }
-
         return view
     }
+
+    private fun fetchCitas(fecha: String) {
+        val url = "${Config.BASE_URL}/cita/get_citas_by_date.php?fecha=$fecha"
+        val client = OkHttpClient()
+
+        val request = Request.Builder().url(url).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                requireActivity().runOnUiThread {
+                    Toast.makeText(requireContext(), "Error al cargar citas", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                requireActivity().runOnUiThread {
+                    if (response.isSuccessful) {
+                        val responseData = response.body?.string()
+                        try {
+                            val jsonResponse = JSONObject(responseData)
+                            val success = jsonResponse.getBoolean("success")
+
+                            if (success) {
+                                val citasArray = jsonResponse.getJSONArray("data")
+                                val citasList = mutableListOf<String>()
+                                for (i in 0 until citasArray.length()) {
+                                    val cita = citasArray.getJSONObject(i)
+                                    val usuario = cita.getString("usuario")
+                                    val mascota = cita.getString("mascota")
+                                    val hora = cita.getString("hora")
+                                    citasList.add("Usuario: $usuario, Mascota: $mascota, Hora: $hora")
+                                }
+
+                                val adapter = ArrayAdapter(
+                                    requireContext(),
+                                    android.R.layout.simple_list_item_1,
+                                    citasList
+                                )
+                                listaCitas.adapter = adapter
+                            } else {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "No hay citas para esta fecha",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                listaCitas.adapter = null
+                            }
+
+                        } catch (e: Exception) {
+                            Toast.makeText(
+                                requireContext(),
+                                "Error al procesar las citas",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            Log.e("CitasAdminFragment", "Error procesando el JSON: ${e.message}")
+                        }
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "No se pudo obtener respuesta del servidor",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        })
+    }
+
 
     private fun iniciarEscaneoQR() {
         val intentIntegrator = IntentIntegrator.forSupportFragment(this)
@@ -92,9 +149,11 @@ class CitasAdminFragment : Fragment() {
         intentIntegrator.setBarcodeImageEnabled(false)
         intentIntegrator.initiateScan()
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        val result: IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        val result: IntentResult =
+            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
             if (result.contents != null) {
                 val qrText = result.contents
@@ -104,23 +163,25 @@ class CitasAdminFragment : Fragment() {
                     val parts = qrText.split(", ")
 
                     // Extraer valores específicos
-                    val nombre = parts[0].split(": ")[1] // Nombre de la mascota
-                    val raza = parts[1].split(": ")[1]   // Raza de la mascota
-                    val edad = parts[2].split(": ")[1]   // Edad de la mascota
+                    val nombre = parts[0].split(": ")[1]
+                    val raza = parts[1].split(": ")[1]
+                    val edad = parts[2].split(": ")[1]
 
-                    // Mostrar en el EditText
+                    // Mostrar en el TextView
                     val text = "Nombre: $nombre\nRaza: $raza\nEdad: $edad"
-                    textFieldCitasEscaneadas.setText(text)
+                    textFieldCitasEscaneadas.text = text
 
                     // Log para depuración
                     Log.e("citaAdministrador", "Nombre: $nombre, Raza: $raza, Edad: $edad")
 
                 } catch (e: Exception) {
-                    // En caso de que el formato no sea el esperado
-                    Toast.makeText(requireContext(), "Error en el formato del QR", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        requireContext(),
+                        "Error en el formato del QR",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     Log.e("Error", "Formato incorrecto en el QR: ${e.message}")
                 }
-
             } else {
                 Toast.makeText(requireContext(), "Escaneo cancelado", Toast.LENGTH_SHORT).show()
             }
